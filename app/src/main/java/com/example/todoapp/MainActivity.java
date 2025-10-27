a330557fcc8f082db0fefe84b60526bbbb7e6e9d
Linhtd <ninggg265@gmail.com>
2025-10-27 23:13:37 +0700

    first commit

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -21,6 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Log.d("TAG", "onCreate: ");
+
     }
 }
